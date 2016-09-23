c456b1f0742c2fba0712e654ed0b9c3fd71aacd2
Rosinka <rosinbass@gmail.com>
2016-09-23 21:41:58 +0300

    Commit

--- src/hw/Work2.java
+++ src/hw/Work2.java
@@ -5,25 +5,42 @@ import java.util.Scanner;
 public class Work2 {
     public static void main(String[] args) {
 
-        int a = 0, b = 0, c;
-
+        int a, b, c;
         int p = 100;
         double fl = 0.99;
         boolean isOK = true;
 
-        //print5(p, fl, isOK);
-        //numbers();
-
-        System.out.println("case 1 - cond\ncase 2 - tern\nanother case - err");
+        System.out.println("case 1 - cond\ncase 2 - tern\ncase 3 - print5\ncase 4 - numbers\ncase 5 - switching\nanother case - err");
         Scanner sc = new Scanner(System.in);
         c = sc.nextInt();
+        System.out.println("a:");
+        a = sc.nextInt();
+        System.out.println("b:");
+        b = sc.nextInt();
 
+
+        switching(a, b, c, p, fl, isOK);
+    }
+
+    public static void switching(int a, int b, int c, int p, double fl, boolean isOK) {
         switch (c) {
             case 1:
                 System.out.println("condAnswer:\n" + cond(a, b));
                 break;
             case 2:
                 System.out.println("ternAnswer:\n" + tern(a, b));
+                break;
+            case 3:
+                System.out.println("Print5:\n");
+                print5(p, fl, isOK);
+                break;
+            case 4:
+                System.out.println("Numbers:\n");
+                numbers();
+                break;
+            case 5:
+                System.out.printf("Switching:\n");
+                switching(a, b, c, p, fl, isOK);
                 break;
             default:
                 System.out.println("err");
@@ -33,12 +50,6 @@ public class Work2 {
 
     public static String cond(int a, int b) {
 
-        System.out.println("a:");
-        Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        System.out.println("b:");
-        b = sc.nextInt();
-
         if ((a - b) < 5) {
             return "<5";
         } else {
@@ -47,19 +58,14 @@ public class Work2 {
     }
 
     public static String tern(int a, int b) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("a:");
-        a = sc.nextInt();
-        System.out.println("b:");
-        b = sc.nextInt();
-        return (a - b) < 5 ? "<5" : ">5";
-    }
 
+        return (a - b) < 5 ? "<5" : ">5";
+
+    }
 
 
     public static void print5(int p, double fl, boolean isOK) {
         int i;
-
         for (i = 0; i < 5; i++) {
             System.out.println(Work1.hw1(p, fl, isOK));
         }
