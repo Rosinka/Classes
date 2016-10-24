842cb357c966a01bb2dabed3a3db068bdcc0faa1
Rosinka <rosinbass@gmail.com>
2016-10-24 22:46:20 +0300

    7 exercises

--- src/hw/Work3.java
+++ src/hw/Work3.java
@@ -10,26 +10,24 @@ public class Work3 {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter array length: ");
-        int size = scan.nextInt();
-        int arr[] = new int[size];
-        System.out.println("Array elements:");
-
-        arrSum(scan, size, arr);
+//        int arr[] = new int[];
+//        System.out.println("Array elements:");
+//
+//        arrSum(scan, arr);
 
         System.out.println("noOst1: ");
         double noOst1 = scan.nextDouble();
         System.out.println("noOst2: ");
         double noOst2 = scan.nextDouble();
 
-        noOst(noOst1, noOst2);
+        noRem(noOst1, noOst2);
 
         System.out.println("even1: ");
         double even1 = scan.nextDouble();
         System.out.println("even2: ");
         double even2 = scan.nextDouble();
 
-        chetko(even1, even2);
+        evenCheck(even1, even2);
 
         System.out.println("char: ");
 
@@ -52,16 +50,22 @@ public class Work3 {
         double d = Math.pow(b, 2) - 4 * a * c;
 
         if (d > 0) {
-            x1 = (-b + sqrt(d)) / 2 * a;
+            x1 = getX1(a, b, d);
             x2 = (-b - sqrt(d)) / 2 * a;
             System.out.println("D > 0\nRoots: x1 = " + x1 + "\n       x2 = " + x2);
         } else if (d == 0) {
             x1 = -b / 2 * a;
-            x2 = -b / 2 * a;
-            System.out.println("D = 0\nRoots: x1 = " + x1 + "\n       x2 = " + x2);
+            System.out.println("D = 0\nRoot: x = " + x1);
         } else {
             System.out.println("D < 0\nNo roots");
         }
+
+    }
+
+    public static double getX1(double a, double b, double d) {
+        double x1;
+        x1 = (-b + sqrt(d)) / 2 * a;
+        return x1;
     }
 
     public static void hasInt(Scanner scan) {
@@ -72,32 +76,20 @@ public class Work3 {
         }
     }
 
-    public static void chetko(double c, double d) {
-        if ((c % 2) == 0 & (d % 2) == 0) {
-            System.out.println("4isla 4etkie");
-        } else if ((c % 1) == 0 & (d % 1) == 0) {
-            System.out.println("4isla ne 4etkie");
-        }
+    public static boolean evenCheck(double c, double d) {
+        return noRem(c, 2) & noRem(d, 2);
     }
 
-    public static void noOst(double a, double b) {
-        if (a % b == 0) {
-            System.out.println("taki da!");
-        } else {
-            System.out.println("nea");
-        }
+    public static boolean noRem(double a, double b) {
+        return (a % b == 0);
     }
 
-    public static void arrSum(Scanner input, int size, int[] arr) {
-        for (int i = 0; i < size; i++) {
-            arr[i] = input.nextInt();
-        }
-
+    public static int arrSum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        System.out.println("Array element sum: " + sum);
+        return sum;
     }
 
 }
---
+++ src/hw/Work4.java
@@ -0,0 +1,129 @@
+package hw;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Work4 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int line1 = scan.nextInt();
+        int line2 = scan.nextInt();
+        int line3 = scan.nextInt();
+
+        exercise1(line1, line2, line3);
+
+        int line4 = scan.nextInt();
+        int line5 = scan.nextInt();
+        int line6 = scan.nextInt();
+
+        exercise2(line4, line5, line6);
+
+        int line7 = scan.nextInt();
+        int line8 = scan.nextInt();
+        int line9 = scan.nextInt();
+        int line10 = scan.nextInt();
+        int line11 = scan.nextInt();
+        int line12 = scan.nextInt();
+
+        exercise3(line7, line8, line9, line10, line11, line12);
+
+        int number1 = scan.nextInt();
+
+        exercise4(number1);
+
+        int number2 = scan.nextInt();
+
+        exercise5(number2);
+
+        int number3 = scan.nextInt();
+
+        exercise6(number3);
+
+        int number4 = scan.nextInt();
+
+        exercise7(number4);
+    }
+
+    public static boolean checkTriangle(double a, double b, double c) {
+        if ((a + b > c) & (a + c > b) & (b + c > a)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean exercise1(double line1, double line2, double line3) {
+
+        if (checkTriangle(line1, line2, line3) == true) {
+            if (line1 == sqrt(Math.pow(line2, 2) + Math.pow(line3, 2)) | line2 == sqrt(Math.pow(line1, 2) + Math.pow(line3, 2)) | line3 == sqrt(Math.pow(line2, 2) + Math.pow(line1, 2))) {
+                return true;
+            } else { return false; }
+        } else { return false; }
+    }
+    
+    public static boolean exercise2(double line4, double line5, double line6) {
+        if (checkTriangle(line4, line5, line6) == true) {
+            if (line4 == line5 | line4 == line6 | line5 == line6) {
+                return true;
+            } else { return false; }
+        } else { return false; }
+    }
+
+    public static boolean exercise3(int line7, int line8, int line9, int line10, int line11, int line12) {
+        if (checkTriangle(line7, line8, line9) == true & checkTriangle(line10, line11, line12) == true) {
+            if ((line7 == line8 & line7 == line9 & line9 == line8) & (line10 == line11 & line10 == line12 & line11 == line12)) {
+                return true;
+            } else { return false; }
+        } else { return false; }
+    }
+
+    public static boolean exercise4(int number1) {
+        int sum = 0;
+        while (number1 > 0){
+            sum += number1 % 10;
+            number1 /= 10;
+        }
+
+        if (sum % 2 == 0) {
+            return true;
+        } else { return false; }
+
+    }
+
+    public static boolean exercise5(int number2) {
+
+        int palindrome = number2;
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        if (number2 == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean exercise6(int number3) {
+
+        if (number3 % 10 + (number3 / 10) % 10 == (number3 / 100) % 10 + (number3 / 1000) % 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean exercise7(int number4) {
+        if ((number4 % 10) > (number4 / 10) % 10 & (number4 / 100) % 10 > (number4 / 1000) % 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
