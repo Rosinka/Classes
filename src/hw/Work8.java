cfb572b3265a7de50555b763d214cf34e69c8bed
Rosinka <rosinbass@gmail.com>
2017-06-04 16:36:33 +0300

    8 work

---
+++ src/hw/Work8.java
@@ -0,0 +1,76 @@
+package hw;
+
+/**
+ * Created by lana on 04.06.17.
+ */
+public class Work8 {
+    public static void main(String[] args) {
+        int[][] grid = gridRandom(5, 5);
+//        searchGridDigit(grid, 5);
+//        copyPartGrid(grid, 2, 2, 3, 3);
+//        System.out.println(mainDiagonalSum(grid));
+//        System.out.println(secondDiagonalAverage(grid));
+
+    }
+
+    public static int[][] gridRandom(int rows, int columns) {
+        int[][] grid = new int[rows][columns];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        printGrid(grid, grid.length, grid[0].length);
+        return grid;
+    }
+
+    private static void printGrid(int[][] grid, int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(grid[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void searchGridDigit(int[][] grid, int searchDigit) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == searchDigit) {
+                    System.out.println(searchDigit + "[" + (i + 1) + "]" + "[" + (j + 1) + "]");
+                }
+            }
+        }
+    }
+
+    public static void copyPartGrid(int[][] grid, int startRow, int startCol, int endRow, int endCol) {
+        int[][] newGrid = new int[grid.length][grid[0].length];
+        for (int i = startRow - 1; i <= endRow - 1; i++) {
+            for (int j = startCol - 1; j <= endCol - 1; j++) {
+                newGrid[i][j] = grid[i][j];
+            }
+        }
+        System.out.println("-----------------");
+        printGrid(newGrid, newGrid.length, newGrid[0].length);
+    }
+
+    public static int mainDiagonalSum(int[][] grid) {
+        int sum = 0;
+        for (int i = 0, j = 0; i < grid.length && j < grid[i].length; i++, j++) {
+            sum += grid[i][j];
+        }
+        return sum;
+    }
+
+    public static double secondDiagonalAverage(int[][] grid) {
+        double sum = 0, count = 0;
+        for (int i = 0, j = grid[0].length - 1; i < grid.length && j >= 0; i++, j--) {
+            sum += grid[i][j];
+            count++;
+        }
+        return sum / count;
+    }
+
+}
+
+
