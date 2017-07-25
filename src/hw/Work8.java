178a8992e26d7cb2b1b470692505e640f7346310
Rosinka <rosinbass@gmail.com>
2017-07-25 16:26:04 +0300

    Work8 - add methods frequencyNumbers, maxGridNumber, secondDiagonalMax

--- src/hw/Work8.java
+++ src/hw/Work8.java
@@ -7,9 +7,9 @@ import java.util.Random;
  */
 public class Work8 {
 
-        private static final int ROWS = 7;
-        private static final int COLUMNS = 7;
-        private static final int RANDOM_MAX_NUMBER = 100;
+    private static final int ROWS = 5;
+    private static final int COLUMNS = 5;
+    private static final int RANDOM_MAX_NUMBER = 5;
 
     public static void main(String[] args) {
 
@@ -19,6 +19,9 @@ public class Work8 {
 //        System.out.println(mainDiagonalSum(grid));
 //        System.out.println(secondDiagonalAverage(grid));
 //        System.out.println(upperTriangleSum(grid));
+//        printGrid(frequencyNumbers(grid));
+//        maxGridNumber(grid);
+//        secondDiagonalMax(grid);
     }
 
     public static int[][] gridRandom() {
@@ -30,13 +33,13 @@ public class Work8 {
                 grid[i][j] = new Random().nextInt(RANDOM_MAX_NUMBER);
             }
         }
-        printGrid(grid, grid.length, grid[0].length);
+        printGrid(grid);
         return grid;
     }
 
-    private static void printGrid(int[][] grid, int rows, int columns) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+    private static void printGrid(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 System.out.print(grid[i][j] + "\t");
             }
             System.out.println();
@@ -61,7 +64,7 @@ public class Work8 {
             }
         }
         System.out.println("-----------------");
-        printGrid(newGrid, newGrid.length, newGrid[0].length);
+        printGrid(newGrid);
     }
 
     public static int mainDiagonalSum(int[][] grid) {
@@ -92,6 +95,60 @@ public class Work8 {
             }
         }
         return sum;
+    }
+
+    public static int[][] frequencyNumbers(int[][] grid) {
+
+        int freq[][] = new int[ROWS * COLUMNS][2];
+        for (int i = 0; i < grid.length; i++) {
+            freq[i][0] = RANDOM_MAX_NUMBER + 1;
+            freq[i][1] = 0;
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                addFreqNumber(freq, grid[i][j]);
+            }
+        }
+        return freq;
+    }
+
+    private static void addFreqNumber(int[][] freq, int value) {
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i][0] == value) {
+                freq[i][1]++;
+                return;
+            }
+        }
+
+        int index = 0;
+        while (freq[index][0] != RANDOM_MAX_NUMBER + 1) {
+            index++;
+        }
+        freq[index][0] = value;
+        freq[index][1] = 1;
+    }
+
+    public static void maxGridNumber(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] > max) {
+                    max = grid[i][j];
+                }
+            }
+        }
+        System.out.println("Max number = " + max);
+    }
+
+    public static void secondDiagonalMax(int[][] grid) {
+        int max = 0;
+        for (int i = 0, j = grid[0].length - 1; i < grid.length && j >= 0; i++, j--) {
+            if (grid[i][j] > max) {
+                max = grid[i][j];
+            }
+        }
+        System.out.println("Max number = " + max);
     }
 
 }
